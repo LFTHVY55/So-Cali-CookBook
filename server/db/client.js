d2ad1ce220b1adc44b604a9befaf78e2fe5e13c5
blvckmac <143541266+blvckmac@users.noreply.github.com>
2024-01-28 18:38:43 -0600

    commit

--- server/db/client.js
+++ server/db/client.js
@@ -1,14 +1,28 @@
 const { Client } = require('pg');
 const dbName = "cookbook"; 
-// const client = new Client(`postgres://localhost:54321/${dbName}`);
+// const client = new Client(`postgres://localhost:5432/${dbName}`);
+
+
+// const client = new Client({
+//   user: "postgres",
+//   host: 'localhost',
+//   database: dbName,
+//   password: "postgres",
+//   port: 5432,
+// });
+
 
 
 const client = new Client({
-  user: "postgres",
-  host: 'localhost',
-  database: dbName,
-  password: "postgres",
-  port: 5432,
+  connectionString: 'postgres://cookbook_9quf_user:vtE4u0ZIji8v5qyfobWtVTXlBYPJ4CD3@dpg-cmqo0jq1hbls73fl785g-a.oregon-postgres.render.com/cookbook_9quf',
+    // user: "cookbook_9quf_user",
+//   host: 'localhost',
+  // database: "cookbook_9quf",
+  // password: "vtE4u0ZIji8v5qyfobWtVTXlBYPJ4CD3",
+  // port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 
